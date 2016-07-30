9e9c852b3fd03358d50023543c61f0d39216ba31
Surkov <SteppenWolf.unity@gmail.com>
2016-07-30 17:23:25 +0300

    fix repo error

--- MyProject/Intermediate/Build/Win64/UE4Editor/Development/MyProject/MyProject.h
+++
@@ -1,5 +0,0 @@
-#ifndef __AUTO_MYPROJECT_H__
-#define __AUTO_MYPROJECT_H__
-//Last Write: 30.07.2016 12:06:09 +00:00
-#include "d:\Unreal Projects\MyProject\Source\MyProject\MyProject.h"
-#endif//__AUTO_MYPROJECT_H__
